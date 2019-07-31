e3be0f4e0450bb148d00bcff8484121fc4f39264
Walber Beltrame <walber.beltrame@gmail.com>
2019-07-31 17:19:32 -0300

    Initial files to csharp supported

---
+++ src/molded.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Molded
+{
+
+ /// <summary>
+ /// The source for a library for simple and fast design pattern of model,
+ /// view and controller in supported modern programming languages.
+ /// <summary>
+ [Author("Walber Antonio Ramos Beltrame (walber.beltrame@gmail.com)")]
+ public abstract class Molded<T> {
+
+  public Molded<T> Molded { get; set; }
+
+  /// <summary>
+  /// The event represents a task of adding some object.
+  /// </summary>
+  public abstract Task<T> Added(T t);
+
+  /// <summary>
+  /// The event represents a task of updating some object.
+  /// </summary>
+  public abstract Task<T> Updated(T t);
+  
+  /// <summary>
+  /// The event represents a task of deleting some object.
+  /// </summary>
+  public abstract Task<T> Deleted(T t);
+
+  /// <summary>
+  /// The event represents a task of reading some object.
+  /// </summary>
+  public abstract Future<T> Readed(T t);
+
+  /// <summary>
+  /// The event represents a task of querying objects.
+  /// </summary>
+  public abstract Task<IEnumerable<T>> Queried(T t = null);
+
+  /// <summary>
+  /// The event represents a task of listening some object.
+  /// </summary>
+  public abstract Task Listened(T t = null);
+
+  /// <summary>
+  /// The event represents a task of unlistening some object.
+  /// </summary>
+  public abstract Task Unlistened(T t = null);
+
+ }
+
+}
