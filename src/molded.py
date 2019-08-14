1a651f336dbc1992711c8549cb94c91670feefbc
Walber Beltrame <walber.beltrame@gmail.com>
2019-08-14 16:05:54 -0300

    Initial python classes in source

---
+++ src/molded.py
@@ -0,0 +1,74 @@
+from abc import ABC, abstractmethod
+
+class Molded(ABC):
+    """
+    The source for a library for simple and fast design pattern of model,
+    view and controller in supported modern programming languages.
+
+    Author
+    ----------
+    Walber Antonio Ramos Beltrame (walber.beltrame@gmail.com)
+    """
+
+    @property
+    def modified(self):
+        """The molded object to a single dispatcher for all modifying events."""
+        return self._modified
+
+    @modified.setter
+    def modified(self, value):
+        self._modified = value
+
+    @abstractmethod
+    async def added(self, value):
+        """The event represents a future of adding some object."""
+        pass
+
+    @abstractmethod
+    async def updated(self, value):
+        """The event represents a future of updating some object."""
+        pass
+
+    @abstractmethod
+    async def deleted(self, value):
+        """The event represents a future of deleting some object."""
+        pass
+
+    @abstractmethod
+    async def readed(self, value):
+        """The event represents a future of reading some object."""
+        pass
+
+    @abstractmethod
+    async def queried(self, value=None):
+        """The event represents a future of querying objects."""
+        pass
+
+    @abstractmethod
+    async def listened(self, value=None):
+        """The event represents a future of listening some object."""
+        pass
+
+    @abstractmethod
+    async def unlistened(self, value=None):
+        """The event represents a future of unlistening some object."""
+        pass
+
+class Modeled(Molded):
+    """A modeled class might have a single listener for all model events in source."""
+    pass
+
+class Viewed(Molded):
+    """A viewed class might have a single listener for all view events in source."""
+    pass
+
+class Controlled(ABC):
+    """The generic class represents a control of events."""
+    def __init__(self, modeled, viewed):
+        """
+        Controlled acts on both modeled and viewed.
+        It controls the data flow into model object and updates the view whenever changes.
+        It keeps view and model separate.
+        """
+        self.modeled = modeled
+        self.viewed = viewed
