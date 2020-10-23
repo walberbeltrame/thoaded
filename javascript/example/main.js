9dff1686d8d7ba946c5e21b15f1e5af0cc2dbe7e
Walber Beltrame <walber.beltrame@gmail.com>
2020-10-23 11:55:10 -0300

    test: simplify test and example

--- javascript/example/main.js
+++ javascript/example/main.js
@@ -7,67 +7,57 @@ const thoaded = require("thoaded");
  */
 class Exampled extends thoaded.Thoaded {
 
- /**
-  * 
-  * Extends thoaded class and make listener for all events.
-  * 
-  */
- constructor() {
-  // run constructor in parent class
-  super();
-  // thoaded object to a single dispatcher for all modifying events
-  this.modified = new thoaded.Thoaded();
-  // make add event
-  this.added = this.promise("added");
-  // make update event
-  this.updated = this.promise("updated");
-  // make delete event
-  this.deleted = this.promise("deleted");
-  // make read event
-  this.readed = this.promise("readed");
-  // make work event
-  this.worked = this.promise("worked");
-  // make handle event
-  this.handled = this.promise("handled");
-  // make query event
-  this.queried = this.promise("queried");
-  // make listen event
-  this.listened = this.promise("listened");
-  // make unlisten event
-  this.unlistened = this.promise("unlistened");
- }
+  /**
+   * 
+   * Extends thoaded class and make listener for all events.
+   * 
+   */
+  constructor() {
+    // run constructor in parent class
+    super();
+    // thoaded object to a single dispatcher for all modifying events
+    this.modified = new thoaded.Thoaded();
+    // make an events list
+    let events = ["added", "updated", "deleted", "readed", "worked",
+      "handled", "queried", "listened", "unlistened"];
+    // for all events
+    for (let index in events) {
+      // make a event with promise
+      this[events[index]] = this.promise(events[index]);
+    }
+  }
 
- /**
+  /**
    * 
    * Make a generic promise
    * 
    * @param {string} event
    * @returns {function} promise
    */
- promise(event) {
-  // make event for promise
-  return (data) => {
-   // make work promise
-   return new Promise((resolve, reject) => {
-    // verify data is valid
-    if (data) {
-     // dispatch modify-based asynchronous event to work data
-     this.modified[event](data).then((result) => {
-      // resolve promise
-      resolve(result);
-     })
-      // exception in event to dispatch data
-      .catch(() => {
-       // reject promise of unsuccessfully
-       reject(data);
+  promise(event) {
+    // make a event for promise
+    return (data) => {
+      // make an work promise
+      return new Promise((resolve, reject) => {
+        // verify data is valid
+        if (data) {
+          // dispatch modify-based asynchronous event to work data
+          this.modified[event](data).then((result) => {
+            // resolve promise
+            resolve(result);
+          })
+            // exception in event to dispatch data
+            .catch(() => {
+              // reject promise of unsuccessfully
+              reject(data);
+            });
+        } else {
+          // reject promise
+          reject(data);
+        }
       });
-    } else {
-     // reject promise
-     reject(data);
     }
-   });
   }
- }
 
 }
 
@@ -78,9 +68,9 @@ class Exampled extends thoaded.Thoaded {
  * 
  */
 class ExampleMoldeled extends Exampled {
- constructor() {
-  super();
- }
+  constructor() {
+    super();
+  }
 }
 
 /**
@@ -90,9 +80,9 @@ class ExampleMoldeled extends Exampled {
  * 
  */
 class ExampleViewed extends Exampled {
- constructor() {
-  super();
- }
+  constructor() {
+    super();
+  }
 }
 
 /**
@@ -102,29 +92,29 @@ class ExampleViewed extends Exampled {
  */
 class ExampleControlled extends thoaded.Controlled {
 
- /**
- * 
- * Extends controlled class and make listener for all events.
- * 
- */
- constructor() {
-  // run constructor in parent class
-  super(new ExampleMoldeled(), new ExampleViewed());
-  // get modeled listener
-  let modeled = this.modeled;
-  // get viewed listener
-  let viewed = this.viewed;
-  // make work event in modeled listener
-  modeled.modified.worked = (data) => {
-   // dispatch handle event to viewed listener
-   return viewed.handled(data);
-  };
-  // make work event in viewed listener
-  viewed.modified.worked = (data) => {
-   // dispatch handle event to modeled listener
-   return modeled.handled(data);
-  };
- }
+  /**
+   * 
+   * Extends controlled class and make listener for all events.
+   * 
+   */
+  constructor() {
+    // run constructor in parent class
+    super(new ExampleMoldeled(), new ExampleViewed());
+    // get modeled listener
+    let modeled = this.modeled;
+    // get viewed listener
+    let viewed = this.viewed;
+    // make a work event in modeled listener
+    modeled.modified.worked = (data) => {
+      // dispatch handle event to viewed listener
+      return viewed.handled(data);
+    };
+    // make a work event in viewed listener
+    viewed.modified.worked = (data) => {
+      // dispatch handle event to modeled listener
+      return modeled.handled(data);
+    };
+  }
 
 }
 
@@ -133,11 +123,11 @@ const exampled = new ExampleControlled();
 
 // dispatch work event to modeled listener
 module.exports = exampled.modeled.worked(Exampled.prototype)
- .then((data) => {
-  // dispatch work event to viewed listener
-  return exampled.viewed.worked(data);
- })
- .then(() => {
-  // return exampled
-  return exampled;
- });
+  .then((data) => {
+    // dispatch work event to viewed listener
+    return exampled.viewed.worked(data);
+  })
+  .then(() => {
+    // return exampled
+    return exampled;
+  });
--- javascript/test/test.js
+++ javascript/test/test.js
@@ -1,5 +1,9 @@
 const thoaded = require("thoaded");
 
+// make an events list
+const events = ["added", "updated", "deleted", "readed", "worked",
+  "handled", "queried", "listened", "unlistened"];
+
 /**
  * 
  * An simple class to test modify-based asynchronous events.
@@ -7,67 +11,54 @@ const thoaded = require("thoaded");
  */
 class Tested extends thoaded.Thoaded {
 
- /**
-  * 
-  * Extends thoaded class and make listener for all events.
-  * 
-  */
- constructor() {
-  // run constructor in parent class
-  super();
-  // thoaded object to a single dispatcher for all modifying events
-  this.modified = new thoaded.Thoaded();
-  // make add event
-  this.added = this.promise("added");
-  // make update event
-  this.updated = this.promise("updated");
-  // make delete event
-  this.deleted = this.promise("deleted");
-  // make read event
-  this.readed = this.promise("readed");
-  // make work event
-  this.worked = this.promise("worked");
-  // make handle event
-  this.handled = this.promise("handled");
-  // make query event
-  this.queried = this.promise("queried");
-  // make listen event
-  this.listened = this.promise("listened");
-  // make unlisten event
-  this.unlistened = this.promise("unlistened");
- }
+  /**
+   * 
+   * Extends thoaded class and make listener for all events.
+   * 
+   */
+  constructor() {
+    // run constructor in parent class
+    super();
+    // thoaded object to a single dispatcher for all modifying events
+    this.modified = new thoaded.Thoaded();
+    // for all events
+    for (let index in events) {
+      // make a event with promise
+      this[events[index]] = this.promise(events[index]);
+    }
+  }
 
- /**
+  /**
    * 
    * Make a generic promise
    * 
    * @param {string} event
    * @returns {function} promise
    */
- promise(event) {
-  // make event for promise
-  return (data) => {
-   // make work promise
-   return new Promise((resolve, reject) => {
-    // verify data is valid
-    if (data) {
-     // dispatch modify-based asynchronous event to work data
-     this.modified[event](data).then((result) => {
-      // resolve promise
-      resolve(result);
-     })
-      // exception in event to dispatch data
-      .catch(() => {
-       // reject promise of unsuccessfully
-       reject(data);
+  promise(event) {
+    // make a event for promise
+    return (data) => {
+      // make an work promise
+      return new Promise((resolve, reject) => {
+        // verify data is valid
+        if (data) {
+          // dispatch modify-based asynchronous event to work data
+          this.modified[event](data).then((result) => {
+            // resolve promise
+            resolve(result);
+          })
+            // exception in event to dispatch data
+            .catch(() => {
+              // reject promise of unsuccessfully
+              reject(data);
+            });
+        } else {
+          // reject promise
+          reject(data);
+        }
       });
-    } else {
-     // reject promise
-     reject(data);
     }
-   });
   }
- }
 
 }
 
@@ -78,9 +69,9 @@ class Tested extends thoaded.Thoaded {
  * 
  */
 class TestMoldeled extends Tested {
- constructor() {
-  super();
- }
+  constructor() {
+    super();
+  }
 }
 
 /**
@@ -90,9 +81,9 @@ class TestMoldeled extends Tested {
  * 
  */
 class TestViewed extends Tested {
- constructor() {
-  super();
- }
+  constructor() {
+    super();
+  }
 }
 
 /**
@@ -102,95 +93,41 @@ class TestViewed extends Tested {
  */
 class TestControlled extends thoaded.Controlled {
 
- /**
- * 
- * Extends controlled class and make listener for all events.
- * 
- */
- constructor() {
-  // run constructor in parent class
-  super(new TestMoldeled(), new TestViewed());
-  // get modeled listener
-  let modeled = this.modeled;
-  // get viewed listener
-  let viewed = this.viewed;
-  // make add event in modeled listener
-  modeled.modified.added = (data) => {
-   // dispatch add event to viewed listener
-   return viewed.added(data);
-  };
-  // make update event in modeled listener
-  modeled.modified.updated = (data) => {
-   // dispatch update event to viewed listener
-   return viewed.updated(data);
-  };
-  // make delete event in modeled listener
-  modeled.modified.deleted = (data) => {
-   // dispatch delete event to viewed listener
-   return viewed.deleted(data);
-  };
-  // make read event in modeled listener
-  modeled.modified.readed = (data) => {
-   // dispatch read event to viewed listener
-   return viewed.readed(data);
-  };
-  // make work event in modeled listener
-  modeled.modified.worked = (data) => {
-   // dispatch work event to viewed listener
-   return viewed.worked(data);
-  };
-  // make handle event in modeled listener
-  modeled.modified.handled = (data) => {
-   // dispatch handle event to viewed listener
-   return viewed.handled(data);
-  };
-  // make query event in modeled listener
-  modeled.modified.queried = (data) => {
-   // dispatch query event to viewed listener
-   return viewed.queried(data);
-  };
-  // make listen event in modeled listener
-  modeled.modified.listened = (data) => {
-   // dispatch listen event to viewed listener
-   return viewed.listened(data);
-  };
-  // make unlisten event in modeled listener
-  modeled.modified.unlistened = (data) => {
-   // dispatch unlisten event to viewed listener
-   return viewed.unlistened(data);
-  };
- }
+  /**
+   * 
+   * Extends controlled class and make listener for all events.
+   * 
+   */
+  constructor() {
+    // run constructor in parent class
+    super(new TestMoldeled(), new TestViewed());
+    // get modeled listener
+    let modeled = this.modeled;
+    // get viewed listener
+    let viewed = this.viewed;
+    // for all events
+    for (let index in events) {
+      // make an event in modeled listener
+      modeled.modified[events[index]] = (data) => {
+        // dispatch event to viewed listener
+        return viewed[events[index]](data);
+      };
+    }
+  }
 
 }
 
 describe("Thoaded", () => {
- const data = Tested.prototype;
- const controlled = new TestControlled();
- it("Added", () => {
-  return controlled.modeled.added(data);
- });
- it("Updated", () => {
-  return controlled.modeled.updated(data);
- });
- it("Deleted", () => {
-  return controlled.modeled.deleted(data);
- });
- it("Readed", () => {
-  return controlled.modeled.readed(data);
- });
- it("Worked", () => {
-  return controlled.modeled.worked(data);
- });
- it("Handled", () => {
-  return controlled.modeled.handled(data);
- });
- it("Queried", () => {
-  return controlled.modeled.queried(data);
- });
- it("Listened", () => {
-  return controlled.modeled.listened(data);
- });
- it("Unlistened", () => {
-  return controlled.modeled.unlistened(data);
- });
+  // make a generic data
+  const data = Tested.prototype;
+  // make a controlled listener
+  const controlled = new TestControlled();
+  // for all events
+  for (let index in events) {
+    // make an event test
+    it(events[index], () => {
+      // dispatch event to modeled listener
+      return controlled.modeled[events[index]](data);
+    });
+  }
 });
